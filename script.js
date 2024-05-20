8c118bc3579915dd811002d46c5c94e2a00b820f
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-20 18:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function stringChop(str, size) {
   // your code here
+	function stringChop(str, size) {
+  // your code here
+	let ans=[];
+	let i=0;
+	let j=size;
+	while(j<str.length){
+		ans.push(str.slice(i,j+1);
+		i=j+1;
+		j= j+size;
+	}
+	ans.push(str.slice(i));
+	return ans;
 }
-
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
