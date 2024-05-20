bedd64468d784b99717e63b16867dd92e78c105c
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-20 18:21:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ function stringChop(str, size) {
   // your code here
 	function stringChop(str, size) {
   // your code here
-	let ans=[];
-	let i=0;
-	let j=size+1;
-	while(j<str.length){
-		ans.push(str.slice(i,j);
-		i=j;
-		j= j+size-1;
-	}
-	ans.push(str.slice(i));
-	return ans;
+	if (size <= 0) return str; 
+    let ans = [];
+    for (let i = 0; i < str.length; i += size) {
+        ans.push(str.slice(i, i + size));
+    }
+    return ans;
 }
 // Do not change the code below
 const str = prompt("Enter String.");
